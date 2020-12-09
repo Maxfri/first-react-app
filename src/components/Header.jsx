509a081eca27d98dc93935adc088d69da342dd01
maxfri <maxfri1995@mail.ru>
2020-12-10 01:38:58 +0300

    Lessons 12

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className="header">
+            <img src="https://www.goldlineindustries.com.au/wp-content/uploads/2019/03/icon-logo.png" alt="" ></img>
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className="content">
+          Main content
+        <div>
+          <img src="https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg"></img>
+        </div>
+        <div>
+          <img src="https://cdn.arstechnica.net/wp-content/uploads/2016/02/5718897981_10faa45ac3_b-640x624.jpg"></img>
+        </div>
+        <div>
+          My post
+          <div>
+            New post
+          </div>
+          <div>
+            <div>
+              post 1
+            </div>
+            <div>
+              post 2
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+}
+
+export default Profile;
