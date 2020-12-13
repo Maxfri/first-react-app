3368f0445e6396083e7cc8757296371403016d0a
maxfri <maxfri1995@mail.ru>
2020-12-14 01:31:57 +0300

    lessons 20

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,26 +1,27 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import classes from './Navbar.module.css';
 
 const Navbar = () => {
-    return (
-        <nav className={classes.nav}>
-        <div className={classes.item}>
-          <a href='/profile'>Profile</a>
-        </div>
-        <div className={classes.item}>
-          <a href='/dialogs'>Messages</a>
-        </div>
-        <div className={classes.item}>
-          <a>News</a>
-        </div>
-        <div className={classes.item}>
-          <a>Music</a>
-        </div>
-        <div className={classes.item}>
-          <a>Settings</a>
-        </div>
-      </nav>
-    );
+  return (
+    <nav className={classes.nav}>
+      <div className={classes.item}>
+        <NavLink to='/profile' activeClassName={classes.activ}>Profile</NavLink>
+      </div>
+      <div className={classes.item}>
+        <NavLink to='/dialogs' activeClassName={classes.activ}>Messages</NavLink>
+      </div>
+      <div className={classes.item}>
+        <a>News</a>
+      </div>
+      <div className={classes.item}>
+        <a>Music</a>
+      </div>
+      <div className={classes.item}>
+        <a>Settings</a>
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
