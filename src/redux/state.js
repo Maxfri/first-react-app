ee88400b63b08110457be51eaac1231f6ca28092
maxfri <maxfri1995@mail.ru>
2020-12-24 00:44:28 +0300

    Lessons 34

--- src/redux/state.js
+++ src/redux/state.js
@@ -5,7 +5,8 @@ let state = {
         posts: [
             { id: 1, message: 'Hello world', likesCount: 12 },
             { id: 2, message: 'Hi, how are you??', likesCount: 100500 }
-        ]
+        ],
+        newPostText: 'Hello jedi!'
     },
     dialogsPage: {
         messages: [
@@ -25,14 +26,19 @@ let state = {
     }
 };
 
-export let addPost = (postMessage) => {
+export let addPost = () => {
     let newPost = {
         id: 5,
-        message: postMessage,
+        message: state.profilePage.newPostText,
         likesCount: 0
     }
 
     state.profilePage.posts.push(newPost);
+    rerenderEntireTree(state);
+}
+
+export let updateNewPostText = (newText) => {
+    state.profilePage.newPostText = newText;
     rerenderEntireTree(state);
 }
 
