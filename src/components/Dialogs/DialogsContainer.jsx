1abd3e4ad82cbb99d374bf7fb7091ec05683a037
maxfri <maxfri1995@mail.ru>
2021-01-19 01:05:36 +0300

    Lessons 45. Connect, mapStateToProps, mapDispatchToProps.

--- src/components/Dialogs/DialogsContainer.jsx
+++ src/components/Dialogs/DialogsContainer.jsx
@@ -1,33 +1,22 @@
 import React from 'react';
 import Dialogs from './Dialogs'
 import { sendMessageCreator, updateNewMessageBodyCreator } from '../../redux/dialog-reducer';
-import StoreContext from '../../StoreContext';
+import { connect } from 'react-redux';
 
-const DialogsContainer = (props) => {
-
-    return (
-        <StoreContext.Consumer>
-            {
-                (store) => {
-                    let state = store.getState().dialogsPage;
-
-                    let onMessageClick = () => {
-                        store.dispatch(sendMessageCreator());
-                    }
-
-                    let onNewMessageChange = (body) => {
-                        store.dispatch(updateNewMessageBodyCreator(body));
-                    }
-
-                    return <Dialogs
-                        updateNewMessageBody={onNewMessageChange}
-                        sendMessage={onMessageClick}
-                        dialogsPage={state}
-                    />
-                }
-            }
-        </StoreContext.Consumer> 
-    )
+let mapStateToProps = (state) => {
+    return {
+        dialogsPage: state.dialogsPage
+    }
 }
+
+
+let mapDispatchToProps = (dispatch) => {
+    return {
+        updateNewMessageBody: (body) => {dispatch(updateNewMessageBodyCreator(body));},
+        sendMessage: () => {dispatch(sendMessageCreator());}
+    }
+}
+
+const DialogsContainer = connect(mapStateToProps, mapDispatchToProps)(Dialogs);
 
 export default DialogsContainer;
