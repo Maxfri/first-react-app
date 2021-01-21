b0e84d71a921396512e63c93962d84c216d1b6c1
maxfri <maxfri1995@mail.ru>
2021-01-22 01:44:37 +0300

    Lessons 52. Users API

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,43 +1,28 @@
+import * as axios from 'axios';
 import React from 'react';
 import styles from './users.module.css';
+import userPhoto from '../../assets/images/unnamed.png'
 
 const Users = (props) => {
-    if (props.users.length === 0) {
-        props.setUsers([
-            {
-                id: 1, photoUrl: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/the-mandalorian-1572282426.jpg?crop=0.8217522658610272xw:1xh;center,top&resize=1200:*',
-                followed: true, fullName: 'Vasya', status: 'Hi, nice to meet you', location: { city: 'Moscow', country: 'Russia' }
-            },
-            {
-                id: 2, photoUrl: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/the-mandalorian-1572282426.jpg?crop=0.8217522658610272xw:1xh;center,top&resize=1200:*',
-                followed: true, fullName: 'Andrew', status: 'Hi, nice to meet you', location: { city: 'Tambov', country: 'Russia' }
-            },
-            {
-                id: 3, photoUrl: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/the-mandalorian-1572282426.jpg?crop=0.8217522658610272xw:1xh;center,top&resize=1200:*',
-                followed: false, fullName: 'Yakov', status: 'Hi, nice to meet you', location: { city: 'Moscow', country: 'Russia' }
-            },
-            {
-                id: 4, photoUrl: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/the-mandalorian-1572282426.jpg?crop=0.8217522658610272xw:1xh;center,top&resize=1200:*',
-                followed: false, fullName: 'Hi, how are you??', status: 'Hi, nice to meet you', location: { city: 'Moscow', country: 'Russia' }
-            },
-            {
-                id: 5, photoUrl: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/the-mandalorian-1572282426.jpg?crop=0.8217522658610272xw:1xh;center,top&resize=1200:*',
-                followed: false, fullName: 'Hello world', status: 'Hi, nice to meet you', location: { city: 'Moscow', country: 'Russia' }
-            },
-            {
-                id: 6, photoUrl: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/the-mandalorian-1572282426.jpg?crop=0.8217522658610272xw:1xh;center,top&resize=1200:*',
-                followed: true, fullName: 'Hi, how are you??', status: 'Hi, nice to meet you', location: { city: 'Moscow', country: 'Russia' }
-            }
-        ])
+    let getUsers = () => {
+        if (props.users.length === 0) {
+
+            axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+                props.setUsers(response.data.items)
+            });
+
+
+        }
     }
 
     return (
         <div>
+            <button onClick={getUsers}>Get Users</button>
             {
                 props.users.map(u => <div key={u.id}>
                     <span>
                         <div>
-                            <img src={u.photoUrl} className={styles.userPhoto} />
+                            <img src={u.photos.small != null ? u.photos.small : userPhoto} className={styles.userPhoto} />
                         </div>
                         {u.followed
                             ? <button onClick={() => { props.unfollow(u.id) }}>Unfollow</button>
@@ -45,12 +30,12 @@ const Users = (props) => {
                     </span>
                     <span>
                         <span>
-                            <div>{u.fullName}</div>
+                            <div>{u.name}</div>
                             <div>{u.status}</div>
                         </span>
                         <span>
-                            <div>{u.location.country}</div>
-                            <div>{u.location.city}</div>
+                            <div>{"u.location.country"}</div>
+                            <div>{"u.location.city"}</div>
                         </span>
                     </span>
                 </div>)
