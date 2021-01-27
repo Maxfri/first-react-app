8edd2b3c93f7605ef1b4be620c1b79f53b84a072
maxfri <maxfri1995@mail.ru>
2021-01-28 02:23:52 +0300

    Lessons 61. Auth.

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -1,4 +1,5 @@
 import { combineReducers, createStore } from "redux";
+import authReducer from "./auth-reducer";
 import dialogReducer from "./dialog-reducer";
 import profileReducer from "./profile-reducer";
 import sidebarReducer from "./sidebar-reducer"
@@ -8,7 +9,8 @@ let reducers = combineReducers({
     profilePage: profileReducer,
     dialogsPage: dialogReducer,
     sidebar: sidebarReducer,
-    usersPage: usersReducer
+    usersPage: usersReducer,
+    auth: authReducer
 });
 
 let store = createStore(reducers);
